c1a643da7d1695ba743aeca560d95bbda8b6745b
Kaz <inakilanusse@gmail.com>
2015-05-26 00:44:04 -0300

    Added population distribution effect

--- Assets/Scripts/RequestSpawner.cs
+++ Assets/Scripts/RequestSpawner.cs
@@ -6,18 +6,42 @@ public class RequestSpawner : MonoBehaviour {
 	public City[] cities;
 	public GameObject prefab;
 	public int maxTupleId;
-	// Use this for initialization
+
+	private float[] probDistribution;
+	private int totalPopulation;
+
 	void Start () {
-		InvokeRepeating ("createRequest", 1, 2);
+		probDistribution = new float[cities.Length];
+		totalPopulation = 0;
+		foreach (City city in cities) {
+			totalPopulation += city.population;
+		}
+		for(int i = 0; i < cities.Length ; i++) {
+			probDistribution[i] = cities[i].population / (float)totalPopulation;
+			if (i != 0) {
+				probDistribution[i] += probDistribution[i - 1];
+			}
+		}
+		InvokeRepeating ("createRequest", 2, 2);
 	}
 
 	void createRequest() {
-		City city = cities [Random.Range (0, cities.Length)];
+		City city = getRandomCity();
 		GameObject requestObj = (GameObject)Instantiate (prefab, city.transform.position, prefab.transform.rotation); 
 		Request request = requestObj.GetComponent<Request> ();
 		request.target = city.closestNode;
 		request.source = city;
 		request.tupleId = Random.Range (0, maxTupleId + 1);
 		request.prefab = false;
+	}
+
+	City getRandomCity() {
+		float value = Random.value;
+		for(int i = 0; i < cities.Length ; i++) {
+			if (value <= probDistribution[i]) {
+				return cities[i];
+			}
+		}
+		return cities [0];
 	}
 }
